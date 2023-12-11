270395d64dd875ae5bdc689b268b541e98cf53bd
MiguelHdezMoreno <miguelanghdez@outlook.com>
2023-12-11 18:50:38 +0000

    Add web scraping functionality to retrieve and parse football league table from Marca website with the help of Selenium to be able to retrieve data from JavaScript

--- WebScrapping/marca_scrap.py
+++ WebScrapping/marca_scrap.py
@@ -1,19 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-import pandas as pd
-import requests
+import csv
 
+# Configurar el driver de Selenium
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
-url = "https://www.marca.com/futbol/primera-division/clasificacion.html?intcmp=MENUMIGA&s_kw=clasificacion"
+# Cargar la página web
+driver.get("https://www.marca.com/futbol/primera-division/clasificacion.html?intcmp=MENUMIGA&s_kw=clasificacion")
 
-# Realizar la solicitud GET al sitio web
-response = requests.get(url)
+# Esperar a que se cargue la tabla de clasificación
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "ue-table-ranking")))
 
-
-# Parsear el contenido HTML de la respuesta
-soup = BeautifulSoup(response.text, "html.parser")
+# Parsear el contenido HTML de la página
+soup = BeautifulSoup(driver.page_source, "html.parser")
 
 # Obtener la tabla de clasificación
-table = soup.find('table', id='ueTableRanking')
+table = soup.find('ue-table-ranking')
 
-print(table)
+# Obtener las filas de la tabla de clasificación
+rows = table.find_all('ue-table-ranking-row')
 
+# Crear una lista para almacenar los datos de clasificación
+clasificacion = []
+
+# Recorrer las filas y obtener los datos de equipo y puntos
+for row in rows:
+    equipo = row.find('ue-table-ranking-team').text.strip()
+    puntos = row.find('ue-table-ranking-points').text.strip()
+    clasificacion.append((equipo, puntos))
+
+# Imprimir los datos de clasificación
+for equipo, puntos in clasificacion:
+    print(f"Equipo: {equipo}, Puntos: {puntos}")
+    
+# Convertir los datos a un .csv
+with open('clasificacion.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Equipo', 'Puntos'])  # Escribir encabezados
+    writer.writerows(clasificacion)  # Escribir datos de clasificación
+
+# Cerrar el driver
+driver.quit()
